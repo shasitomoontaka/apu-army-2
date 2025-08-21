67dc9ab29d777f10fa2c7187f18c6e7f5d9c8dad
builderbergertester <shasitomoontaka@tutanota.com>
2025-08-21 10:31:22 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://shasitomoontaka.github.io/apu-army-2/',
 	integrations: [mdx(), sitemap()],
 });
