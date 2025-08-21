0e72ce9f1d2ca2ff8a0157c91cded49a70dd91b8
builderbergertester <shasitomoontaka@tutanota.com>
2025-08-21 14:04:05 +0200

    url

--- src/helper/url.js
+++ src/helper/url.js
@@ -2,5 +2,5 @@
 export const base = import.meta.env.BASE_URL.replace(/\/$/, '');
 
 export function url(path) {
-  return `${base}${path.startsWith('/') ? path : `/${path}`}`;
+  return `${base}${path.startsWith('/apu-army-2/') ? path : `/${path}`}`;
 }
