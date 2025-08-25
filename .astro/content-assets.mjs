1c3ab017127867a10c8f607353ab20b8ff20ad73
shasitomoontaka <shasitomoontaka@tutanota.com>
2025-08-25 23:18:03 +0200

    Update content-assets.mjs

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,8 +1,8 @@
 
-import __ASTRO_IMAGE_IMPORT_mofUD from "../../assets/peace.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuvil-war.md";
 import __ASTRO_IMAGE_IMPORT_rHNvT from "../../assets/whale.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapu-whale-collection.md";
-import __ASTRO_IMAGE_IMPORT_1E2HY8 from "../../assets/apu-army.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuarmy.md";
-import __ASTRO_IMAGE_IMPORT_1OFt5L from "../../assets/blewstein-files.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fblewstein.md";
 import __ASTRO_IMAGE_IMPORT_Z2waKis from "../../assets/blew.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpondtalk.mdx";
-export default new Map([["../../assets/peace.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuvil-war.md", __ASTRO_IMAGE_IMPORT_mofUD], ["../../assets/whale.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapu-whale-collection.md", __ASTRO_IMAGE_IMPORT_rHNvT], ["../../assets/apu-army.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuarmy.md", __ASTRO_IMAGE_IMPORT_1E2HY8], ["../../assets/blewstein-files.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fblewstein.md", __ASTRO_IMAGE_IMPORT_1OFt5L], ["../../assets/blew.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpondtalk.mdx", __ASTRO_IMAGE_IMPORT_Z2waKis]]);
+import __ASTRO_IMAGE_IMPORT_1OFt5L from "../../assets/blewstein-files.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fblewstein.md";
+import __ASTRO_IMAGE_IMPORT_1E2HY8 from "../../assets/apu-army.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuarmy.md";
+import __ASTRO_IMAGE_IMPORT_mofUD from "../../assets/peace.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuvil-war.md";
+export default new Map([["../../assets/whale.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapu-whale-collection.md", __ASTRO_IMAGE_IMPORT_rHNvT], ["../../assets/blew.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpondtalk.mdx", __ASTRO_IMAGE_IMPORT_Z2waKis], ["../../assets/blewstein-files.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fblewstein.md", __ASTRO_IMAGE_IMPORT_1OFt5L], ["../../assets/apu-army.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuarmy.md", __ASTRO_IMAGE_IMPORT_1E2HY8], ["../../assets/peace.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fapuvil-war.md", __ASTRO_IMAGE_IMPORT_mofUD]]);
 		
